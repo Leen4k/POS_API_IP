107bc356284e31ff1fbaf5581b62aae7ee38fe0e
wathnak <110277798+Leen4k@users.noreply.github.com>
2024-06-21 19:15:38 +0700

    add cors policy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('POS_API')
     .setDescription('POS_API documentation')
