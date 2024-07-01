a15be45e7ebc8458309aea5d3833c1d2bf7a5f41
wathnak <110277798+Leen4k@users.noreply.github.com>
2024-07-01 08:16:23 +0700

    update get order

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,33 +1,51 @@
-import { BadRequestException, ForbiddenException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class OrderService {
-  constructor(private readonly prisma: PrismaService){}
-  
+  constructor(private readonly prisma: PrismaService) {}
+
   async createOrder(createOrderDto: CreateOrderDto) {
     const { customerId, employeeId, paidCash, orderDetails } = createOrderDto;
-  
+
     // Validate customer and employee existence
-    const customer = await this.prisma.customer.findUnique({ where: { id: customerId } });
+    const customer = await this.prisma.customer.findUnique({
+      where: { id: customerId },
+    });
     if (!customer) throw new NotFoundException('Customer not found');
-    
-    const employee = await this.prisma.employee.findUnique({ where: { id: employeeId } });
+
+    const employee = await this.prisma.employee.findUnique({
+      where: { id: employeeId },
+    });
     if (!employee) throw new NotFoundException('Employee not found');
-  
+
     // Calculate total price and validate product availability
     let totalPrice = 0;
     const orderItemsWithPrice = [];
-  
+
     for (const orderItem of orderDetails) {
       if (orderItem.quantity <= 0) {
         throw new BadRequestException('Quantity must be greater than zero');
       }
-      const product = await this.prisma.product.findUnique({ where: { id: orderItem.productId } });
-      if (!product || !product.isAvailable || product.stock < orderItem.quantity) {
-        throw new BadRequestException(`Product ${orderItem.productId} is not available or out of stock`);
+      const product = await this.prisma.product.findUnique({
+        where: { id: orderItem.productId },
+      });
+      if (
+        !product ||
+        !product.isAvailable ||
+        product.stock < orderItem.quantity
+      ) {
+        throw new BadRequestException(
+          `Product ${orderItem.productId} is not available or out of stock`,
+        );
       }
       totalPrice += product.price * orderItem.quantity;
       orderItemsWithPrice.push({
@@ -35,13 +53,13 @@ export class OrderService {
         price: product.price,
       });
     }
-  
+
     if (paidCash < totalPrice) {
       throw new BadRequestException('Paid cash is less than total price');
     }
-  
+
     const returnCash = paidCash - totalPrice;
-  
+
     // Create order, order details, and purchase history in a transaction
     const order = await this.prisma.$transaction(async (prisma) => {
       const newOrder = await prisma.order.create({
@@ -59,7 +77,7 @@ export class OrderService {
           },
         },
       });
-  
+
       // Create purchase history records
       await prisma.purchaseHistory.createMany({
         data: orderItemsWithPrice.map((orderItem) => ({
@@ -69,7 +87,7 @@ export class OrderService {
           price: orderItem.price,
         })),
       });
-  
+
       // Update product stock
       for (const orderItem of orderItemsWithPrice) {
         await prisma.product.update({
@@ -77,10 +95,10 @@ export class OrderService {
           data: { stock: { decrement: orderItem.quantity } },
         });
       }
-  
+
       return newOrder;
     });
-  
+
     return order;
   }
 
@@ -91,9 +109,16 @@ export class OrderService {
         skip,
         take: Number(size || 10),
         include: {
-          orderDetails: true,
+          orderDetails: {
+            include: {
+              product: true,
+            },
+          },
           customer: true,
           employee: true,
+        },
+        orderBy: {
+          purchaseDate: 'desc',
         },
       });
 
@@ -110,8 +135,8 @@ export class OrderService {
         },
       };
     } catch (error) {
-      console.log(error)
-      throw new InternalServerErrorException("Cannot fetch orders");
+      console.log(error);
+      throw new InternalServerErrorException('Cannot fetch orders');
     }
   }
 
@@ -141,23 +166,26 @@ export class OrderService {
       // Fetch the existing order
       const order = await this.prisma.order.findUnique({
         where: { id },
-        include: { orderDetails: true }
+        include: { orderDetails: true },
       });
-  
+
       if (!order) {
-        throw new NotFoundException("Order not found");
+        throw new NotFoundException('Order not found');
       }
-  
+
       // Calculate the time difference
       const createdAt = new Date(order.purchaseDate);
       const currentTime = new Date();
-      const timeDifferenceInMinutes = (currentTime.getTime() - createdAt.getTime()) / (1000 * 60);
-  
+      const timeDifferenceInMinutes =
+        (currentTime.getTime() - createdAt.getTime()) / (1000 * 60);
+
       // Check if the time difference exceeds the maximum allowed time (10 minutes)
       if (timeDifferenceInMinutes > 10) {
-        throw new ForbiddenException("Order cannot be deleted after 10 minutes of creation");
+        throw new ForbiddenException(
+          'Order cannot be deleted after 10 minutes of creation',
+        );
       }
-  
+
       // Start a transaction to ensure atomicity
       await this.prisma.$transaction(async (prisma) => {
         // Increment product quantity back
@@ -166,17 +194,17 @@ export class OrderService {
             where: { id: detail.productId },
             data: {
               stock: {
-                increment: detail.quantity
-              }
-            }
+                increment: detail.quantity,
+              },
+            },
           });
         }
-  
+
         // Delete order details
         await prisma.orderDetail.deleteMany({
-          where: { orderId: id }
+          where: { orderId: id },
         });
-  
+
         // Delete associated purchase history
         // await prisma.purchaseHistory.deleteMany({
         //   where: {
@@ -184,16 +212,16 @@ export class OrderService {
         //     productId: { in: order.orderDetails.map(detail => detail.productId) }
         //   }
         // });
-  
+
         // Delete the order
         await prisma.order.delete({
-          where: { id }
+          where: { id },
         });
       });
-  
     } catch (error) {
-      throw new InternalServerErrorException(error.message || "Cannot delete order");
+      throw new InternalServerErrorException(
+        error.message || 'Cannot delete order',
+      );
     }
   }
-  
 }
